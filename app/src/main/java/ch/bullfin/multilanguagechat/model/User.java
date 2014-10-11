98e735bfd9af7bef0c34dc3f3d1c2c1d41f2e4c3
Rahul Raveendran <rahul@bullfin.ch>
2014-10-11 21:34:08 +0530

    user class made singleton

--- app/src/main/java/ch/bullfin/multilanguagechat/model/User.java
+++ app/src/main/java/ch/bullfin/multilanguagechat/model/User.java
@@ -1,12 +1,47 @@
 package ch.bullfin.multilanguagechat.model;
 
+import android.content.Context;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
 /**
  * Created by root on 11/10/14.
  */
 public class User {
+    private static final String PREF_FILE = "UserFile";
+    private static final String PREF_KEY = "UserKey";
+
     private long id;
     private String name;
     private String email;
+
+    private static User instance = null;
+
+    public static User getInstance(Context context) {
+        if (instance == null) {
+            String json = context.getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE)
+                    .getString(PREF_KEY, null);
+            try {
+                instance = new Gson().fromJson(json, User.class);
+            } catch (JsonSyntaxException e) {
+                e.getStackTrace();
+            }
+
+            if (instance == null) {
+                instance = new User();
+            }
+        }
+
+        return instance;
+    }
+
+    public void save(Context context) {
+        context.getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE)
+                .edit()
+                .putString(PREF_KEY, new Gson().toJson(this))
+                .commit();
+    }
 
     public long getId() {
         return id;
