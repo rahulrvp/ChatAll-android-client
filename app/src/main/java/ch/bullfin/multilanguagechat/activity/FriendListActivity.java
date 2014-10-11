bba93a4caf62a374c9e1c5a6987db6c27ebe8426
sreejithk <sreejithk@bullfin.ch>
2014-10-11 21:05:53 +0530

    friends  list design done

--- app/src/main/java/ch/bullfin/multilanguagechat/activity/FriendListActivity.java
+++ app/src/main/java/ch/bullfin/multilanguagechat/activity/FriendListActivity.java
@@ -1,18 +1,28 @@
 package ch.bullfin.multilanguagechat.activity;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+import ch.bullfin.httpmanager.HTTPManager;
+import ch.bullfin.httpmanager.Response;
 import ch.bullfin.multilanguagechat.R;
 import ch.bullfin.multilanguagechat.adapter.FriendListAdapter;
+import ch.bullfin.multilanguagechat.model.User;
 
 public class FriendListActivity extends BaseActivity {
 
+    private ProgressBar mPropertySpinner;
+    private ArrayList<User> mFriendList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +30,8 @@ public class FriendListActivity extends BaseActivity {
 
         FriendListAdapter friendListAdapter = new FriendListAdapter(mContext);
         ListView friendList = (ListView) findViewById(R.id.friends_list);
+        mPropertySpinner = (ProgressBar) findViewById(R.id.friends_list_progress);
+        mFriendList = new ArrayList<User>();
 
         if (friendList != null) {
             friendList.setAdapter(friendListAdapter);
@@ -29,5 +41,41 @@ public class FriendListActivity extends BaseActivity {
     public void onChatsClicked(View view) {
         Intent intent = new Intent(mContext, MyChatsActivity.class);
         startActivity(intent);
+    }
+
+    private class FetchFriendListTask extends AsyncTask<Void, Void, ArrayList<User>> {
+
+        @Override
+        protected void onPreExecute() {
+            if (mPropertySpinner != null) {
+                mPropertySpinner.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        protected ArrayList<User> doInBackground(Void... voids) {
+            Gson gson = new Gson();
+            HashMap<String,String> params = new HashMap<String, String>();
+            params.put("user_id", "1");
+
+            Response response = new HTTPManager("URL").get(params);
+
+            if (response.getStatusCode() == 200) {
+                User[] users = gson.fromJson(response.getResponseBody(), User[].class);
+                mFriendList = new ArrayList<User>();
+                Collections.addAll(mFriendList, users);
+
+
+            }
+
+            return mFriendList;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<User> result) {
+            if (mPropertySpinner != null) {
+                mPropertySpinner.setVisibility(View.GONE);
+            }
+        }
     }
 }
