f431ca2565de1ff3fab5095ddcb517fa2fbacf89
Rahul Raveendran <rahul@bullfin.ch>
2014-10-11 16:51:00 +0530

    added message item layout and message detail activity

---
+++ app/src/main/java/ch/bullfin/multilanguagechat/activity/ChatDetailsActivity.java
@@ -0,0 +1,18 @@
+package ch.bullfin.multilanguagechat.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import ch.bullfin.multilanguagechat.R;
+
+/**
+ * Created by root on 11/10/14.
+ */
+public class ChatDetailsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_details);
+    }
+}
