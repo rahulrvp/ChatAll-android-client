0fbffebecb98650f7c5a46b189b117d818a1be57
sreejithk <sreejithk@bullfin.ch>
2014-10-11 23:59:48 +0530

    sign up done

--- app/src/main/java/ch/bullfin/multilanguagechat/model/User.java
+++ app/src/main/java/ch/bullfin/multilanguagechat/model/User.java
@@ -7,6 +7,7 @@ public class User {
     private long id;
     private String name;
     private String email;
+    private String phoneNumber;
 
     public long getId() {
         return id;
@@ -30,5 +31,13 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
