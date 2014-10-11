51666aaab168ffa4293c51e69db505d21959af62
meera <meera@bullfin.ch>
2014-10-11 19:16:58 +0530

    chat_cell

--- app/src/main/java/ch/bullfin/multilanguagechat/activity/MyChatsActivity.java
+++ app/src/main/java/ch/bullfin/multilanguagechat/activity/MyChatsActivity.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 
 import ch.bullfin.multilanguagechat.R;
+import ch.bullfin.multilanguagechat.adapter.ChatListAdapter;
+import ch.bullfin.multilanguagechat.adapter.FriendListAdapter;
 
 public class MyChatsActivity extends BaseActivity {
 
@@ -15,6 +18,13 @@ public class MyChatsActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_chats);
+
+        ChatListAdapter chatListAdapter = new ChatListAdapter(mContext);
+        ListView chatList = (ListView) findViewById(R.id.friends_list);
+
+        if (chatList != null) {
+            chatList.setAdapter(chatListAdapter);
+        }
     }
 
     public void onFriendsClicked(View view) {
---
+++ app/src/main/java/ch/bullfin/multilanguagechat/adapter/ChatListAdapter.java
@@ -0,0 +1,84 @@
+package ch.bullfin.multilanguagechat.adapter;
+
+import android.content.Context;
+import android.text.format.Time;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import ch.bullfin.multilanguagechat.R;
+import ch.bullfin.multilanguagechat.activity.model.Chat;
+import ch.bullfin.multilanguagechat.model.User;
+
+/**
+ * Created by meera on 11/10/14.
+ */
+public class ChatListAdapter extends BaseAdapter {
+
+    Context mContext;
+    private ArrayList<Chat> mchats;
+
+    public ChatListAdapter(Context context) {
+        this.mContext = context;
+        mchats = new ArrayList<Chat>();
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public Chat getItem(int position) {
+        return mchats.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        ViewHolder viewHolder = null;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.chat_list_item, null, false);
+
+            if (convertView == null) return null;
+
+            viewHolder = new ViewHolder();
+            viewHolder.prof_pic = (ImageView) convertView.findViewById(R.id.friend_image);
+            viewHolder.name = (TextView) convertView.findViewById(R.id.friend_name);
+            viewHolder.time = (TextView) convertView.findViewById(R.id.time);
+            viewHolder.chatmsg = (TextView) convertView.findViewById(R.id.msg);
+            convertView.setTag(viewHolder);
+
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+            if (viewHolder != null) {
+                viewHolder.name.setText("Meera");
+                viewHolder.time.setText("7.00pm");
+                viewHolder.chatmsg.setText("hello");
+            }
+//        Fill data to List Cell
+        return convertView;
+    }
+
+    private class ViewHolder {
+        TextView name;
+        ImageView prof_pic;
+        TextView time;
+        TextView chatmsg;
+    }
+
+    public void setValues(ArrayList<Chat> enquires) {
+        mchats = enquires;
+        notifyDataSetChanged();
+    }
+}
