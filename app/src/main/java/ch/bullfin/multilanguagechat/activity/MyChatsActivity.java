31c56c15932d754bc82a5e1cabb58ade86252b69
Rahul Raveendran <rahul@bullfin.ch>
2014-10-12 06:10:41 +0530

    refresh chat-list in one second

--- app/src/main/java/ch/bullfin/multilanguagechat/activity/MyChatsActivity.java
+++ app/src/main/java/ch/bullfin/multilanguagechat/activity/MyChatsActivity.java
@@ -1,7 +1,9 @@
 package ch.bullfin.multilanguagechat.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -13,18 +15,26 @@ import ch.bullfin.multilanguagechat.model.Chat;
 
 public class MyChatsActivity extends BaseActivity {
 
+    private Handler mHandler;
+    private Context context;
+    private ChatListAdapter mChatListAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_chats);
 
-        final ChatListAdapter chatListAdapter = new ChatListAdapter(mContext);
+        context = this;
+
+        mHandler = new Handler();
+
+        mChatListAdapter = new ChatListAdapter(mContext);
         ListView chatList = (ListView) findViewById(R.id.friends_list);
 
         new FetchchatsTask(mContext, new FetchchatsTask.FetchChatsCallback() {
             @Override
             public void onFetchchatsCompleted(Chat[] chat) {
-                chatListAdapter.setValues(chat);
+                mChatListAdapter.setValues(chat);
             }
 
             @Override
@@ -34,12 +44,12 @@ public class MyChatsActivity extends BaseActivity {
         }).execute();
 
         if (chatList != null) {
-            chatList.setAdapter(chatListAdapter);
+            chatList.setAdapter(mChatListAdapter);
             chatList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                     Intent intent = new Intent(mContext, ChatDetailsActivity.class);
-                    intent.putExtra("chat", chatListAdapter.getItem(i));
+                    intent.putExtra("chat", mChatListAdapter.getItem(i));
                     startActivity(intent);
                 }
             });
@@ -50,4 +60,39 @@ public class MyChatsActivity extends BaseActivity {
         Intent intent = new Intent(mContext, FriendListActivity.class);
         startActivity(intent);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mHandler != null) {
+            mHandler.post(fetchMyChatsRunnable);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mHandler != null) {
+            mHandler.removeCallbacks(fetchMyChatsRunnable);
+        }
+    }
+
+    private Runnable fetchMyChatsRunnable = new Runnable() {
+        @Override
+        public void run() {
+            new FetchchatsTask(context, new FetchchatsTask.FetchChatsCallback() {
+                @Override
+                public void onFetchchatsCompleted(Chat[] chat) {
+                    mChatListAdapter.setValues(chat);
+                }
+
+                @Override
+                public void onFetchchatsFailed() {
+
+                }
+            }).execute();
+
+            mHandler.postDelayed(fetchMyChatsRunnable, 1000);
+        }
+    };
 }
